383a28aa494b130048fe832799c22dc6608a4f31
yamachu <yamachu.dev@gmail.com>
2024-09-26 18:38:46 +0900

    Add macOS Sequoia workaround

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -19,6 +19,15 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
+#if MACCATALYST
+		// https://github.com/dotnet/maui/issues/23390#issuecomment-2202295194
+		var handlerType = typeof(Microsoft.Maui.Handlers.HybridWebViewHandler);
+		var field = handlerType.GetField("AppOriginUri",
+			System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic
+		) ?? throw new Exception("AppOriginUri field not found");
+		field.SetValue(null, new Uri("app://localhost/"));
+#endif
+
 		return builder.Build();
 	}
 }
